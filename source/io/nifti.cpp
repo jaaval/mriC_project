91aa39aebf78aad4bcc7eb84100dee53cffb4d43
jaakko vallinoja <jaakko@localhost.localdomain>
2018-01-24 02:03:19 +0200

    more testing binary readng

--- source/io/nifti.cpp
+++ source/io/nifti.cpp
@@ -1,134 +1,215 @@
 
 #include "nifti.h"
+#include <iostream>
 
 
 Nifti::Nifti(std::string fileName) {
+  header1 = nifti_1_header();
+  header2 = nifti_2_header();
+  this->fileName = fileName;
+  std::ifstream ifs (fileName, std::ifstream::binary);
+  if (ifs) {
+    checkDataType(ifs);
+    readHeaderType1(ifs, swap);
+    ifs.close();
+  } else {
+    std::cout << "shit 3" << std::endl;
+  }
+}
+
+
+void Nifti::checkDataType(std::ifstream& ifs) {
+  char* buffer4 = new char[4];
+  ifs.get(buffer4, 5);
+  int a = cast_to_int(buffer4, false);
+  if (a == 348) {
+    type = 1;
+    swap = false;
+  } else if (a == 540) {
+    type = 2;
+    swap = false;
+  } else {
+    a = cast_to_int(buffer4, true);
+    if (a == 348) {
+      type = 1;
+      swap = true;
+    } else if (a == 540) {
+      type = 2;
+      swap = true;
+    } else {
+      std::cout << "fuck this" << std::endl;
+      // ERROR!
+    }
+  }
+
+  delete[] buffer4;
+  ifs.seekg(0, ifs.beg);
+}
+
+
+void Nifti::readFile(std::string fileName) {
 
 }
 
-Nifti::readHeader(ifstream& ifs) {
-  char buffer4[4];
-  char buffer2[2];
 
-  ifs.get(buffer4, 4);
-  this->header.sizeof_hdr = cast_to_int(buffer4);
-  if (this->header.sizeof_hdr != 348) {
+void Nifti::readData(std::ifstream& ifs) {
+
+}
+
+
+void Nifti::readHeaderType1(std::ifstream& ifs, bool swap) {
+  char* buffer4 = new char[4];
+  char* buffer2 = new char[2];
+
+  ifs.get(buffer4, 5);
+  header1.sizeof_hdr = cast_to_int(buffer4, swap);
+  if (header1.sizeof_hdr != 348) {
     // The end of the world!
+    std::cout << "shit" << std::endl;
   }
-  ifs.get(this->header.data_type, 10);
-  ifs.get(this->header.db_name, 18);
 
-  ifs.get(buffer4, 4);
-  this->header.extents = cast_to_int(buffer4);
+  ifs.get(header1.data_type, 11);
 
-  ifs.get(buffer2, 2);
-  this->header.session_error = cast_to_short(buffer2);
+  ifs.get(header1.db_name, 19);
 
-  ifs.get(this->header.regular);
+  ifs.get(buffer4, 5);
+  header1.extents = cast_to_int(buffer4, swap);
 
-  ifs.get(this->header.dim_info);
+  ifs.get(buffer2, 3);
+  header1.session_error = cast_to_short(buffer2, swap);
+
+  ifs.get(header1.regular);
+
+  ifs.get(header1.dim_info);
 
   for (int i = 0; i < 8; i++) {
-      ifs.get(buffer2, 2);
-      this->header.dim[i] = cast_to_short(buffer2);
+      ifs.get(buffer2, 3);
+      header1.dim[i] = cast_to_short(buffer2, swap);
   }
 
-  for (int i = 0; i < 3; i++) {
-    ifs.get(buffer4, 4);
-    this->header.intent[i] = cast_to_float(buffer4);
-  }
+  ifs.get(buffer4, 5);
+  header1.intent_p1 = cast_to_float(buffer4, swap);
+  ifs.get(buffer4, 5);
+  header1.intent_p2 = cast_to_float(buffer4, swap);
+  ifs.get(buffer4, 5);
+  header1.intent_p3 = cast_to_float(buffer4, swap);
 
-  ifs.get(buffer2, 2);
-  this->header.intent_code[i] = cast_to_short(buffer2);
+  ifs.get(buffer2, 3);
+  header1.intent_code = cast_to_short(buffer2, swap);
 
-  ifs.get(buffer2, 2);
-  this->header.datatype[i] = cast_to_short(buffer2);
+  ifs.get(buffer2, 3);
+  header1.datatype = cast_to_short(buffer2, swap);
 
-  ifs.get(buffer2, 2);
-  this->header.bitpix[i] = cast_to_short(buffer2);
+  ifs.get(buffer2, 3);
+  header1.bitpix = cast_to_short(buffer2, swap);
 
-  ifs.get(buffer2, 2);
-  this->header.slice_start[i] = cast_to_short(buffer2);
+  ifs.get(buffer2, 3);
+  header1.slice_start = cast_to_short(buffer2, swap);
 
   for (int i = 0; i < 8; i++) {
-    ifs.get(buffer4, 4);
-    this->header.pixdim[i] = cast_to_float(buffer4);
+    ifs.get(buffer4, 5);
+    header1.pixdim[i] = cast_to_float(buffer4, swap);
   }
 
-  ifs.get(buffer4, 4);
-  this->header.vox_offset = cast_to_float(buffer4);
+  ifs.get(buffer4, 5);
+  header1.vox_offset = cast_to_float(buffer4, swap);
 
-  ifs.get(buffer4, 4);
-  this->header.slc_slope = cast_to_float(buffer4);
+  ifs.get(buffer4, 5);
+  header1.scl_slope = cast_to_float(buffer4, swap);
 
-  ifs.get(buffer4, 4);
-  this->header.slc_inter = cast_to_float(buffer4);
+  ifs.get(buffer4, 5);
+  header1.scl_inter = cast_to_float(buffer4, swap);
 
-  ifs.get(buffer2, 2);
-  this->header.slice_end[i] = cast_to_short(buffer2);
+  ifs.get(buffer2, 3);
+  header1.slice_end = cast_to_short(buffer2, swap);
 
-  ifs.get(this->header.slice_code);
+  ifs.get(header1.slice_code);
 
-  ifs.get(this->header.xyzt_units);
+  ifs.get(header1.xyzt_units);
 
-  ifs.get(buffer4, 4);
-  this->header.cal_max = cast_to_float(buffer4);
+  ifs.get(buffer4, 5);
+  header1.cal_max = cast_to_float(buffer4, swap);
 
-  ifs.get(buffer4, 4);
-  this->header.cal_min = cast_to_float(buffer4);
+  ifs.get(buffer4, 5);
+  header1.cal_min = cast_to_float(buffer4, swap);
 
-  ifs.get(buffer4, 4);
-  this->header.slice_duration = cast_to_float(buffer4);
+  ifs.get(buffer4, 5);
+  header1.slice_duration = cast_to_float(buffer4, swap);
 
-  ifs.get(buffer4, 4);
-  this->header.toffset = cast_to_float(buffer4);
+  ifs.get(buffer4, 5);
+  header1.toffset = cast_to_float(buffer4, swap);
 
-  ifs.get(buffer4, 4);
-  this->header.glmax = cast_to_int(buffer4);
+  ifs.get(buffer4, 5);
+  header1.glmax = cast_to_int(buffer4, swap);
 
-  ifs.get(buffer4, 4);
-  this->header.glmin = cast_to_int(buffer4);
+  ifs.get(buffer4, 5);
+  header1.glmin = cast_to_int(buffer4, swap);
 
-  ifs.get(this->header.descrip, 80);
+  ifs.get(header1.descrip, 81);
 
-  ifs.get(this->header.aux_file, 24);
+  ifs.get(header1.aux_file, 25);
 
-  ifs.get(buffer2, 2);
-  this->header.qform_code[i] = cast_to_short(buffer2);
+  ifs.get(buffer2, 3);
+  header1.qform_code = cast_to_short(buffer2, swap);
 
-  ifs.get(buffer2, 2);
-  this->header.sform_code[i] = cast_to_short(buffer2);
+  ifs.get(buffer2, 3);
+  header1.sform_code = cast_to_short(buffer2, swap);
 
-  for (int i = 0; i < 3; i++) {
-    ifs.get(buffer4, 4);
-    this->header.quatern[i] = cast_to_float(buffer4);
-  }
 
-  for (int i = 0; i < 3; i++) {
-    ifs.get(buffer4, 4);
-    this->header.qoffset[i] = cast_to_float(buffer4);
+  ifs.get(buffer4, 5);
+  header1.quatern_b = cast_to_float(buffer4, swap);
+  ifs.get(buffer4, 5);
+  header1.quatern_c = cast_to_float(buffer4, swap);
+  ifs.get(buffer4, 5);
+  header1.quatern_d = cast_to_float(buffer4, swap);
+
+  ifs.get(buffer4, 5);
+  header1.qoffset_x = cast_to_float(buffer4, swap);
+  ifs.get(buffer4, 5);
+  header1.qoffset_y = cast_to_float(buffer4, swap);
+  ifs.get(buffer4, 5);
+  header1.qoffset_z = cast_to_float(buffer4, swap);
+
+
+  for (int i = 0; i < 4; i++) {
+    ifs.get(buffer4, 5);
+    header1.srow_x[i] = cast_to_float(buffer4, swap);
   }
 
   for (int i = 0; i < 4; i++) {
-    ifs.get(buffer4, 4);
-    this->header.srow_x[i] = cast_to_float(buffer4);
+    ifs.get(buffer4, 5);
+    header1.srow_y[i] = cast_to_float(buffer4, swap);
   }
 
   for (int i = 0; i < 4; i++) {
-    ifs.get(buffer4, 4);
-    this->header.srow_y[i] = cast_to_float(buffer4);
+    ifs.get(buffer4, 5);
+    header1.srow_z[i] = cast_to_float(buffer4, swap);
   }
 
-  for (int i = 0; i < 4; i++) {
-    ifs.get(buffer4, 4);
-    this->header.srow_z[i] = cast_to_float(buffer4);
-  }
+  ifs.get(header1.intent_name, 17);
 
-  ifs.get(this->header.intent_name, 16);
+  ifs.get(header1.magic, 5);
 
-  ifs.get(this->header.magic, 4);
-
-  if (ifs.tellg != 348) {
+  if (ifs.tellg() != 348) {
     //well shit
+    std::cout << "shit 2 " << ifs.tellg() << std::endl;
+
   }
+
+  delete[] buffer2;
+  delete[] buffer4;
+}
+
+
+int main() {
+  std::string filename = "fPhantom-0005-00001-000001-01.nii";
+  Nifti ni = Nifti(filename);
+  std::cout << ni.getHeader().sizeof_hdr << std::endl;
+
+  for (int i = 0; i < 8; i++) {
+    std::cout << ni.getHeader().dim[i] << std::endl;
+  }
+  std::cout << ni.getHeader().descrip << std::endl;
+
+
 }
--- source/io/nifti.h
+++ source/io/nifti.h
@@ -1,8 +1,12 @@
-
+#ifndef NIFTI_H
+#define NIFTI_H
 
 
 #include <fstream>
 #include <string>
+
+#include "nifti1.h"
+#include "nifti2.h"
 
 
 class Nifti {
@@ -10,21 +14,33 @@ public:
 
   Nifti(const std::string fileName);
 
-  NiftiHeader& getHeader() const {
-    return header;
+  nifti_1_header& getHeader() {
+    return header1;
   }
 
-  size_t dimx() const {
-    return (size_t) header.dim[0];
+  short dimx() const {
+    if (type == 1)
+      return header1.dim[0];
+    else
+      return header2.dim[0];
   }
-  size_t dimy() const {
-    return (size_t) header.dim[1];
+  short dimy() const {
+    if (type == 1)
+      return header1.dim[1];
+    else
+      return header2.dim[1];
   }
-  size_t dimz() const {
-    return (size_t) header.dim[2];
+  short dimz() const {
+    if (type == 1)
+      return header1.dim[2];
+    else
+      return header2.dim[2];
   }
-  size_t dimt() const {
-    return (size_t) header.dim[3];
+  short dimt() const {
+    if (type == 1)
+      return header1.dim[3];
+    else
+      return header2.dim[3];
   }
 
   void readFile(std::string fileName);
@@ -32,54 +48,20 @@ public:
 
 private:
 
-  void readHeader(ifstream& ifs);
-  void readData(ifstream& ifs, size_t bytes); //TODO think how to actually read the data
+  void readHeaderType1(std::ifstream& ifs, bool swap);
+  void readHeaderType2(std::ifstream& ifs, bool swap);
+  void readData(std::ifstream& ifs);
+  void checkDataType(std::ifstream& ifs);
 
-  NiftiHeader header;
+  nifti_1_header header1;
+  nifti_2_header header2;
+  int type;
   bool dataLoaded;
+  bool swap;
   std::string fileName;
 
 };
 
-struct NiftiHeader {
-  int sizeof_hdr; // header size == 348
-  char data_type[10]; // not used
-  char db_name[18]; // not used
-  int extents; // not used
-  short session_error; // not used
-  char regular; // not used
-  char dim_info; // encodings for directions
-  short dim[8]; // data dimensions
-  float intent[3]; // intent parameters p1,p2,p3
-  short intent_code; // nifti intent
-  short datatype;
-  short bitpix; // number of bits / voxel
-  short slice_start; // first slice index
-  float pixdim[8]; // grid spacings (unit per dimension)
-  float vox_offset; // offset ??
-  float scl_slope; // data scaling, slope
-  float scl_inter; // data scaling, offset  -> slope*x+inter
-  short slice_end; // last slice index
-  char slice_code; //slice timing order?
-  char xyzt_units; // units of pixdim[1:4]
-  float cal_max; // max disp intensity
-  float cal_min; // min disp intensity
-  float slice_duration; // time for one slice (TR?)
-  float toffset; // time axis shift
-  int glmax; // not used
-  int glmin; // not used
-  char descrip[80]; // description text
-  char aux_file[24]; // aux file name
-  short qform_code; // use the quaternion fields
-  short sform_code; // use the affine fields
-  float quatern[3]; // quaternion bcd parameters
-  float qoffset[3]; // quaternion offsets
-  float srow_x[4]; // 1st row affine transform
-  float srow_y[4]; // 2st row affine transform
-  float srow_z[4]; // 3st row affine transform
-  char intent_name[16]; // name or meaning of the data
-  char magic[4]; //magic string ?
-};
 
 class Volume {
 
@@ -92,14 +74,32 @@ private:
 
 // cast functions for binary data reading
 
-int cast_to_int(char[4] buffer4) {
-  return (int) (buffer4[0] << 24 | buffer4[1] << 16 | buffer4[2] << 8 | buffer4[3]);
+int cast_to_int(char* buffer4, bool swap) {
+  int a;
+  if (swap) {
+    a = (int) (buffer4[0] << 24 | buffer4[1] << 16 | buffer4[2] << 8 | buffer4[3]);
+  } else {
+    a = (int) (buffer4[3] << 24 | buffer4[2] << 16 | buffer4[1] << 8 | buffer4[0]);
+  }
+  return a;
 }
 
-float cast_to_float(char[4] buffer4) {
-  return (float) (buffer4[0] << 24 | buffer4[1] << 16 | buffer4[2] << 8 | buffer4[3]);
+float cast_to_float(char* buffer4, bool swap) {
+  return (float) (buffer4[3] << 24 | buffer4[2] << 16 | buffer4[1] << 8 | buffer4[0]);
 }
 
-short cast_to_short(char[2] buffer2) {
-  return (short) (buffer2[0] << 8 | buffer2[1]);
+short cast_to_short(char* buffer2, bool swap) {
+  short a;
+  if (swap) {
+    a = (short) (buffer2[0] << 8 | buffer2[1]);
+  } else {
+    a =  (short) (buffer2[1] << 8 | buffer2[0]);
+  }
+  return a;
 }
+
+
+
+
+
+#endif
