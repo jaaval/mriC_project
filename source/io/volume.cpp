492312df120b3697e0033cfdb58502ef2ed7dee5
jaakko vallinoja <jaakko@localhost.localdomain>
2018-01-26 03:40:37 +0200

    Basic index ops on volume works

--- source/io/volume.cpp
+++ source/io/volume.cpp
@@ -1,5 +1,6 @@
 
 #include "volume.h"
+#include <iostream>
 
 
 double Volume::get(int i, int t) {
@@ -16,17 +17,60 @@ std::vector<double>& Volume::get(int x, int y, int z) {
   return data[index];
 }
 
-std::vector<int> Volume::getNeighborhoodIndices(int i, int r) {
-  int[] xyz = i_to_xyz(i, dims);
-  int size = (2*r+1) * (2*r+1) * (2*r+1);
-  int indices[size];
-  int counter = 0;
-  for (int a = xyz[0] - r; a < xyz[0] + r+1; a++) {
-    for (int b = xyz[1] - r; b < xyz[1] + r+1; b++) {
-      for (int c = xyz[2] - r; c < xyz[2] + r+1; c++) {
-        indices[counter++] = xyz_to_i(a, b, c, dims);
+std::vector<int> Volume::getNeighborhoodIndices(int x, int y, int z, int r) {
+  std::vector<int> indices;
+  for (int a = x - r; a < x + r+1; a++) {
+    for (int b = y - r; b < y + r+1; b++) {
+      for (int c = z - r; c < z + r+1; c++) {
+        if (a == x+r || a == x-r || b == y+r || b == y-r || c == z+r || c == z-r) {
+          if (a >= 0 && b >= 0 && c >= 0 && a < dims[0] && b < dims[1] && c < dims[2]) {
+            indices.push_back(xyz_to_i(a, b, c, dims));
+          }
+        }
       }
     }
   }
-  return std::vector<int> v(indices, size)
+  return indices;
+}
+
+std::vector<int> Volume::getNeighborhoodIndices(int i, int r) {
+  std::vector<int> xyz = i_to_xyz(i, dims);
+  return getNeighborhoodIndices(xyz[0], xyz[1], xyz[2], r);
+}
+
+Volume::Volume() {//test constructor
+  dims[0] = 64;
+  dims[1] = 64;
+  dims[2] = 48;
+  dims[3] = 5;
+  for (int i = 0; i < 64*64*48; i++) {
+    std::vector<double> datatest = {i,i,i,i,i};
+    data.push_back(datatest);
+  }
+}
+
+
+
+
+int main() {
+  int num = 25*25*25;
+  Volume asdf;
+  double test1 = asdf.get(num,2);
+  int ds[] = {64,64,48, 5};
+  std::vector<int> bla = i_to_xyz(num, ds);
+  double test2 = asdf.get(bla[0], bla[1], bla[2], 2);
+  std::cout << test1 << std::endl;
+  std::cout << test2 << std::endl;
+  std::cout << bla[0] << " " << bla[1] << " " << bla[2] << std::endl;
+  std::vector<int> bla2 = asdf.getNeighborhoodIndices(num, 2);
+  for (auto iter = bla2.begin(); iter < bla2.end(); iter++) {
+    std::cout << *iter << std::endl;
+  }
+  std::cout << "---------------------" << std::endl;
+  std::vector<int> bla3 = asdf.getNeighborhoodIndices(num, 1);
+  std::vector<int> bla4;
+  for (auto iter = bla3.begin(); iter < bla3.end(); iter++) {
+    bla4 = i_to_xyz(*iter, ds);
+    std::cout << bla4[0] << " " << bla4[1] << " " << bla4[2] << std::endl;
+  }
 }
--- source/io/volume.h
+++ source/io/volume.h
@@ -2,45 +2,47 @@
 #define VOLUME_H
 
 #include <vector>
+#include <assert.h>
 
 typedef struct VolInfo {
   int a;//placeholder
 } VolInfo;
 
-int xyz_to_i(int x, int y, int z, int[] dims) {
+int xyz_to_i(int x, int y, int z, int dims[]) {
   assert(x < dims[0] &&  y < dims[1] && z < dims[2]);
   return x * dims[1] * dims[2] + y * dims[2] + z;
 }
 
-int[] i_to_xyz(int i, int[] dims) {
+std::vector<int> i_to_xyz(int i, int dims[]) {
   assert(i < dims[0] * dims[1] * dims[2]);
   int yz = dims[1] * dims[2];
   int x = i / yz;
   int ix = (i - x * yz);
   int y = ix / dims[2];
   int z = ix % dims[2];
-  return int[] {x,y,z};
+  return std::vector<int> {x,y,z};
 }
 
 class Volume {
 
 public:
 
+  Volume();
 
-double get(int i, int t);
-double get(int x, int y, int z, int t);
-std::vector<double>& get(int x, int y, int z);
+  double get(int i, int t);
+  double get(int x, int y, int z, int t);
+  std::vector<double>& get(int x, int y, int z);
 
-std::vector<int> getNeighborhoodIndices(int i, int r);
-std::vector<int> getNeighborhoodIndices(int x, int y, int z, int r);
+  std::vector<int> getNeighborhoodIndices(int i, int r);
+  std::vector<int> getNeighborhoodIndices(int x, int y, int z, int r);
 
 private:
 
-// data x*y*z*t
-std::vector<std::vector<double>> data;
-VolInfo info;
+  // data x*y*z*t
+  std::vector<std::vector<double>> data;
+  VolInfo info;
 
-int dims[4];
+  int dims[4];
 
 };
 
